aeb872a366890f7dbaebc2c4c82e38f76d272349
asher.rulez <asher.rulez@gmail.com>
2016-10-15 23:43:18 +0300

    added exclude to interface

--- app/src/main/java/DataModel/IFirebaseSavable.java
+++ app/src/main/java/DataModel/IFirebaseSavable.java
@@ -1,8 +1,11 @@
 package DataModel;
 
+import com.google.firebase.database.Exclude;
+
 /**
  * Created by Asher on 21.08.2016.
  */
 public interface IFirebaseSavable {
+    @Exclude
     int getSavableClassType();
 }
